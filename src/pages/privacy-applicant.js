b487c1c890eb78ac1cc6c24b1b71d66ade0d5b6e
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 17:38:45 +0800

    Updated privacy-applicant.js

--- src/pages/privacy-applicant.js
+++ src/pages/privacy-applicant.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'privacy-applicant',
+  pageDescription: 'Applicant\'s Privacy',
+}
+
 
 const PrivacyApplicantPage = () => (
   <div>
-    privacy applicant page
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
