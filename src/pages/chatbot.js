1c5991c32a1f7af40883828b2a140eaef788d169
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 16:53:52 +0800

    updated chatbot.js

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'chatbot',
+  pageDescription: 'Our chatbots creates Augmented Recruiters by automating every recruitment step from scheduling interviews, to collecting referrals, to onboarding new hires.',
+}
 
 const ChatbotPage = () => (
   <div>
-    chatbot page
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
