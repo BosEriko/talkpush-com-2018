6682efd0f0c7de5bbb4d2ba97c2332c238c5f98f
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 17:30:33 +0800

    removed the iframe-chatbot link on navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -14,7 +14,6 @@ const Navigation = () => (
       <li><Link to="/crm">CRM</Link></li>
       <li><Link to="/customers">Customers</Link></li>
       <li><Link to="/genpact-demo">Genpact Demo</Link></li>
-      <li><Link to="/iframe-chatbot">iFrame Chatbot</Link></li>
       <li><Link to="/">Index</Link></li>
       <li><Link to="/jobs">Jobs</Link></li>
       <li><Link to="/privacy-applicant">Privacy Applicant</Link></li>
