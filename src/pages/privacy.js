06d2a8e456f9d39f30edd9cadd8e1c52fa17abdb
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 17:41:45 +0800

    Updated privacy and terms

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'privacy',
+  pageDescription: 'In accordance with international data privacy laws, our privacy laws protect all candidate data from being shared, accessed or sold outside of their consent.',
+}
+
 
 const PrivacyPage = () => (
   <div>
-    privacy page
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'terms',
+  pageDescription: 'Terms and Conditions',
+}
+
 
 const TermsPage = () => (
   <div>
-    terms page
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
