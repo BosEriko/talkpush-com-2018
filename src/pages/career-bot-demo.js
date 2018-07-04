a5f66ce754e7da1c5eb850ae1438383523ec4e97
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 16:52:25 +0800

    career-bot-demo updated

--- src/pages/career-bot-demo.js
+++ src/pages/career-bot-demo.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'career-bot-demo',
+  pageDescription: 'Join a global team of disruptors, creators and futurists. Help define how machines and humans will interact tomorrow on the matter of recruitment.',
+}
 
 const CareerBotDemoPage = () => (
   <div>
-    career bot demo page 
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
