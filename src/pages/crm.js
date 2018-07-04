26261c1d12ac5e1c782001870e63af0d328b24aa
Bos Eriko <bos.eriko@gmail.com>
2018-07-04 17:13:42 +0800

    Updated crm.js

--- src/pages/crm.js
+++ src/pages/crm.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const pageVariables = {
+  pageCode: 'crm',
+  animationSpeed: '800',
+  animationOnce: 'true',
+  pageDescription: 'Candidate Relationship Management for the messaging age',
+}
 
 const CrmPage = () => (
   <div>
-    crm page
+    <Helmet>
+      <title>{pageVariables.pageCode}</title>
+      <meta name='description' content={pageVariables.pageDescription} />
+      <meta property='og:description' content={pageVariables.pageDescription} />
+    </Helmet>
+    <p>{pageVariables.pageDescription}</p>
   </div>
 )
 
