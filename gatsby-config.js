94b44c0700176bc8ed78fa8859f06da4a540ac4e
Bos Eriko <bos.eriko@gmail.com>
2018-07-12 13:14:49 +0800

    Moved google analytics to a plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title:          'Talkpush',
-    description:    'Let your talent talk | Recruitment Chatbot Platform',
-    metaKeywords:   'Talkpush, Chat Application, Recruitment Platform, Messaging, Recruitment Chatbot',
-    image:          './og-image.jpg',
+    title: 'Talkpush',
+    description: 'Let your talent talk | Recruitment Chatbot Platform',
+    metaKeywords: 'Talkpush, Chat Application, Recruitment Platform, Messaging, Recruitment Chatbot',
+    image: './og-image.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-46858485-2",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+  ],
 }
