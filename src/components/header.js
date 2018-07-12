0857b547c25d5b21901d0ecf5e8e5c1dad0b7011
Bos Eriko <bos.eriko@gmail.com>
2018-07-12 18:10:32 +0800

    Header scroll now working

--- src/components/header.js
+++ src/components/header.js
@@ -2,22 +2,36 @@ import React from 'react'
 import Navigation from './navigation'
 import './style/header.scss'
 
-const Header = ({ title }) => (
-  <header>
-    <div className="container">
-      <div className="divider">
-        <div className="logo">
-          <a className="logo" href="#top-section">
-            <span>{title} Logo</span>
-          </a>
-          <a className="menu-button" href="#">
-            <i className="fa fa-bars" aria-hidden="true"></i>
-          </a>
+class Header extends React.Component {
+  componentDidMount() {
+    window.onscroll = () => {
+      if (document.scrollingElement.scrollTop >= 50) {
+        document.getElementById("header").classList.add("active")
+      }
+      else {
+        document.getElementById("header").classList.remove("active")
+      }
+    }
+  }
+  render() {
+    return (
+      <header id="header">
+        <div className="container">
+          <div className="divider">
+            <div className="logo">
+              <a className="logo" href="#top-section">
+                <span>{this.props.title} Logo</span>
+              </a>
+              <a className="menu-button" href="#">
+                <i className="fa fa-bars" aria-hidden="true"></i>
+              </a>
+            </div>
+            <Navigation />
+          </div>
         </div>
-        <Navigation />
-      </div>
-    </div>
-  </header>
-)
+      </header>
+    );
+  }
+}
 
 export default Header
