3e517f1709f1f4fe3343f271133381f209c6b1d0
Bos Eriko <bos.eriko@gmail.com>
2018-07-19 19:46:29 +0800

    Added text on the first block

--- src/pages-split/index/talk-to-your-candidate.js
+++ src/pages-split/index/talk-to-your-candidate.js
@@ -2,10 +2,21 @@ import React from 'react'
 import styles from './style/talk-to-your-candidate.module.scss'
 
 const TalkToYourCandidateIndexPageSplit = () => (
-  <div className={styles.block} style={{ paddingTop: '200px', paddingLeft: '100px' }}>
-    <a href="#globalButton" className="global-button global-button-block">Global Button</a>
-    <h1 className="global-hero-glow-white">White Glow</h1>
-    <h1 className="global-hero-glow-green">Green Glow</h1>
+  <div className={styles.block}>
+    <div className={styles.fillLeft}></div>
+    <div className={styles.content}>
+      <div className={styles.contentLeft}>
+      {/* Content Start */}
+        <div className={styles.contentLeftContent}>
+          <h1 className="global-hero-glow-white text-uppercase m-0 p-0">Talk To Your</h1>
+          <h1 className="global-hero-glow-green text-uppercase m-0 p-0">Candidate</h1>
+          <p>Recruiting is pretty simple: get your recruiters and your candidates to talk to each other and watch good things happen. Talkpush is the first conversation-driven Candidate Relationship Management (CRM) system. On Talkpush, recruiters spend more time talking to qualified candidates, which translates into a better candidate experience and huge reductions in cost-per-hire and time-to-fill.</p>
+        </div>
+      {/* Content End */}
+      </div>
+      <div className={styles.contentRight}></div>
+    </div>
+    <div className={styles.fillRight}></div>
   </div>
 )
 
