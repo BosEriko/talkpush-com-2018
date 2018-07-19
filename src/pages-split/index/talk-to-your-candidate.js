8bb7a367f709727cfa8dfbbccdb19739ab796562
Bos Eriko <bos.eriko@gmail.com>
2018-07-19 16:25:45 +0800

    Updated the purple color and used an scss variable

--- src/pages-split/index/talk-to-your-candidate.js
+++ src/pages-split/index/talk-to-your-candidate.js
@@ -4,6 +4,8 @@ import styles from './style/talk-to-your-candidate.module.scss'
 const TalkToYourCandidateIndexPageSplit = () => (
   <div className={styles.block} style={{ paddingTop: '200px', paddingLeft: '100px' }}>
     <a href="#globalButton" className="global-button global-button-block">Global Button</a>
+    <h1 className="global-hero-glow-white">White Glow</h1>
+    <h1 className="global-hero-glow-green">Green Glow</h1>
   </div>
 )
 
