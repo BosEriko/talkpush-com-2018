affbbfeab6b1f0394e4d61e812f58ba088562e84
Bos Eriko <bos.eriko@gmail.com>
2018-07-23 14:03:29 +0800

    update on the crm section

--- src/pages-split/index/crm.js
+++ src/pages-split/index/crm.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './style/crm.module.scss'
 import crmIcon from '../../icons/crm-white.svg'
+import laptopActive from './images/crm/laptop-active.svg'
+import laptop from './images/crm/laptop.svg'
+import mobileActive from './images/crm/mobile-active.svg'
+import mobile from './images/crm/mobile.svg'
 
 const CrmIndexPageSplit = () => (
   <div className={styles.block}>
     <div className="container">
       <div className={styles.blockDivider}>
-        <div className={styles.blockDividerLeft}>1</div>
+        <div className={styles.blockDividerLeft}>
+          <img id="index-page-crm-block-laptop-active"  className={styles.blockDividerLeftLaptopActive}   src={laptopActive}  alt="Laptop Active"   title="Laptop Active"/>
+          <img id="index-page-crm-block-laptop"         className={styles.blockDividerLeftLaptop}         src={laptop}        alt="Laptop"          title="Laptop"/>
+          <img id="index-page-crm-block-mobile-active"  className={styles.blockDividerLeftMobileActive}   src={mobileActive}  alt="Mobile Active"   title="Mobile Active"/>
+          <img id="index-page-crm-block-mobile"         className={styles.blockDividerLeftMobile}         src={mobile}        alt="Mobile"          title="Mobile"/>
+        </div>
         <div className={styles.blockDividerRight}>
           <h2 className="global-hero-glow-white m-0 p-0">Finally a CRM that<br />Lets You Talk</h2>
           <p>Recruitment is not about tracking, it's about talking. Talkpush initiates thousands of conversations between recruiters and candidates via chat, social media and voice to build a pre-qualified talent pool that is engaged and responsive.</p>
