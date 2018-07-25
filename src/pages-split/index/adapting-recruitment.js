b8302a2518ba944a9179bd953d0e6f594b5ce78b
Bos Eriko <bos.eriko@gmail.com>
2018-07-25 15:50:58 +0800

    Finished the adapting block

--- src/pages-split/index/adapting-recruitment.js
+++ src/pages-split/index/adapting-recruitment.js
@@ -11,22 +11,22 @@ const AdaptingRecruitmentIndexPageSplit = () => (
       <h4 className="p-0">Meet your candidates where they live and create engaging digital recruitment experiences</h4>
       <div className={styles.blockDivider}>
         <div className={styles.blockDividerItem}>
-          <img src={fasterIcon} alt="Faster Application" title="Faster Application" />
+          <div className={styles.blockDividerItemImage}><img src={fasterIcon} alt="Faster Application" title="Faster Application" /></div>
           <h3 className="global-hero-glow-white m-0 p-0">Makes applications faster</h3>
           <p>In less than 5 minutes, candidates can apply, complete a first screening, and be told if they are shortlisted. This used to take days.</p>
-          <a href="#" className="global-button text-lowercase">Read More</a>
+          <div className={styles.blockDividerItemLink}><a href="#" className="global-button text-lowercase">Read More</a></div>
         </div>
         <div className={styles.blockDividerItem}>
-          <img src={conversationIcon} alt="Keep it Going" title="Keep it Going" />
+          <div className={styles.blockDividerItemImage}><img src={conversationIcon} alt="Keep it Going" title="Keep it Going" /></div>
           <h3 className="global-hero-glow-white m-0 p-0">Keep the conversation going</h3>
           <p>Once a conversation is engaged with a candidate via Talkpush, it lives on. Using SMS and messaging, employers get a 99% read rate and 70%+ response rate.</p>
-          <a href="#" className="global-button text-lowercase">Read More</a>
+          <div className={styles.blockDividerItemLink}><a href="#" className="global-button text-lowercase">Read More</a></div>
         </div>
         <div className={styles.blockDividerItem}>
-          <img src={multiplatformIcon} alt="Multiplatform" title="Multiplatform" />
+          <div className={styles.blockDividerItemImage}><img src={multiplatformIcon} alt="Multiplatform" title="Multiplatform" /></div>
           <h3 className="global-hero-glow-white m-0 p-0">Meet them where they live</h3>
           <p>Talkpush allows you to engage a conversation with candidates on the platform they live on: Facebook Messenger, WeChat, Line, etc.</p>
-          <a href="#" className="global-button text-lowercase">Read More</a>
+          <div className={styles.blockDividerItemLink}><a href="#" className="global-button text-lowercase">Read More</a></div>
         </div>
       </div>
     </div>
