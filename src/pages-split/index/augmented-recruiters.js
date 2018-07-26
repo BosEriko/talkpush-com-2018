174684f6967e4d4108440b1a52323363599ca2cd
Bos Eriko <bos.eriko@gmail.com>
2018-07-26 17:39:28 +0800

    Added the title and subtitle

--- src/pages-split/index/augmented-recruiters.js
+++ src/pages-split/index/augmented-recruiters.js
@@ -3,7 +3,10 @@ import styles from './style/augmented-recruiters.module.scss'
 
 const AugmentedRecruitersIndexPageSplit = () => (
   <div className={styles.block}>
-    <div className="container">test again</div>
+    <div className="container">
+      <h2 className="global-hero-glow-white m-0 p-0">Meet the Augmented Recruiters</h2>
+      <h4 className="p-0">They build more engaging experiences and increase recruitment outputs through automation and AI</h4>
+    </div>
   </div>
 )
 
