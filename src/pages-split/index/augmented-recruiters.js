94e9f452a271d9e1798af4366ee009d809e4e8e1
Bos Eriko <bos.eriko@gmail.com>
2018-07-26 17:51:38 +0800

    updated the react component of augmented-recruiters.js

--- src/pages-split/index/augmented-recruiters.js
+++ src/pages-split/index/augmented-recruiters.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from './style/augmented-recruiters.module.scss'
 
-const AugmentedRecruitersIndexPageSplit = () => (
-  <div className={styles.block}>
-    <div className="container">
-      <h2 className="global-hero-glow-white m-0 p-0">Meet the Augmented Recruiters</h2>
-      <h4 className="p-0">They build more engaging experiences and increase recruitment outputs through automation and AI</h4>
-    </div>
-  </div>
-)
+class AugmentedRecruitersIndexPageSplit extends React.Component {
+  render() {
+    return (
+      <div className={styles.block}>
+        <div className="container">
+          <h2 className="global-hero-glow-white m-0 p-0">Meet the Augmented Recruiters</h2>
+          <h4 className="p-0">They build more engaging experiences and increase recruitment outputs through automation and AI</h4>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default AugmentedRecruitersIndexPageSplit
