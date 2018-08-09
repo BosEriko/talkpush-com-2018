e9a5ea937a5238f81c1ab8c81daaffc4a390344b
Bos Eriko <bos.eriko@gmail.com>
2018-08-09 19:24:20 +0800

    Added header to crm

--- src/pages/crm.js
+++ src/pages/crm.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styles from '../pages-style/crm.module.scss'
+import conversationsImage from '../pages-images/crm/conversations.png'
 
 const pageVariables = {
   pageCode: 'crm',
-  animationSpeed: '800',
-  animationOnce: 'true',
   pageDescription: 'Candidate Relationship Management for the messaging age',
 }
 
 const CrmPage = () => (
-  <div>
+  <div className={styles.block}>
     <Helmet>
       <title>{pageVariables.pageCode}</title>
       <meta name='description' content={pageVariables.pageDescription} />
       <meta property='og:description' content={pageVariables.pageDescription} />
     </Helmet>
+    <div className={styles.blockHeader}>
+      <div className={styles.blockHeaderContent}>
+        <div className="container">
+          <h1 className="global-hero-glow-white">A CRM designed for <span>Messaging and Voice</span></h1>
+        </div>
+      </div>
+    </div>
+    {/* <p><img src={conversationsImage} alt="Conversations"/></p> */}
     <p>{pageVariables.pageDescription}</p>
   </div>
 )
