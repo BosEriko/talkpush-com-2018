6e5029256e9f286802ea4e32f680626c9421c701
Bos Eriko <bos.eriko@gmail.com>
2018-08-13 15:14:48 +0800

    made a responsive header for the jobs page

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styles from '../pages-style/jobs.module.scss'
+// import hiringIcon from '../pages-images/jobs/hiring.svg'
+// import cultureIcon from '../pages-images/jobs/culture.svg'
+// import locationIcon from '../pages-images/jobs/location.svg'
 
 class JobsPage extends React.Component {
   pageVariables = {
@@ -42,10 +45,26 @@ class JobsPage extends React.Component {
             <div className={styles.blockHeaderContentFill}></div>
             <h1 className="global-hero-glow-white p-0 m-0">We're Hiring</h1>
             <p className="p-0">Thinking about working at Talkpush? We want to learn more about you so we can decide if there is a potential career for you at Talkpush. Talk to our recruitment agent Stanley below and start that conversation NOW.</p>
+            <div className={styles.blockHeaderContentLink}>
+              <a href="#" className="global-button">
+                <i className="fa fa-users fa-fw" aria-hidden="true"></i>
+                <span>Hiring Process</span>
+              </a>
+              <a href="#" className="global-button">
+                <i className="fa fa-coffee fa-fw" aria-hidden="true"></i>
+                <span>Our Culture</span>
+              </a>
+              <a href="#" className="global-button">
+                <i className="fa fa-globe fa-fw" aria-hidden="true"></i>
+                <span>Our Location</span>
+              </a>
+            </div>
             <div className={styles.blockHeaderContentFill}></div>
           </div>
         </div>
-        <iframe src="/talkpush-bot/index.html" style={{ width: '100%' }} scrolling="no" frameBorder="0" id="talkpush-bot-iframe"></iframe>
+        <div className={styles.blockChat}>
+          <iframe src="/talkpush-bot/index.html" style={{ width: '100%' }} scrolling="no" frameBorder="0" id="talkpush-bot-iframe"></iframe>
+        </div>
       </div>
     )
   }
