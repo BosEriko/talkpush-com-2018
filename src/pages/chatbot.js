86084e63f40c4d4eb4e811cd2750a0b49442c81b
Bos Eriko <bos.eriko@gmail.com>
2018-08-14 20:39:44 +0800

    so much stuff left

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -2,11 +2,30 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import styles from '../pages-style/chatbot.module.scss'
 import logoIcon from '../pages-images/chatbot/logo.svg'
+import conversation from '../pages-images/chatbot/conversation.svg'
+import context from '../pages-images/chatbot/context.svg'
+import moreThanWords from '../pages-images/chatbot/more-than-words.svg'
 
 class ChatbotPage extends React.Component {
   pageVariables = {
     pageCode: 'chatbot',
     pageDescription: 'Our chatbots creates Augmented Recruiters by automating every recruitment step from scheduling interviews, to collecting referrals, to onboarding new hires.',
+  }
+
+  pageItem = (reverse, photo, title, paragraph) => {
+    return (
+      <div className={styles.blockContentItem} style={reverse === true ? { flexDirection: "row-reverse" } : null}>
+        <div className={reverse === true ? styles.blockContentItemImageReverse : styles.blockContentItemImage}>
+          {photo ? <img src={photo} alt={title} /> : <div><iframe width="560" height="315" src="https://www.youtube.com/embed/FQ2iZ4qvcg4" frameBorder="0" allowFullScreen=""></iframe></div>}
+        </div>
+        <div className={styles.blockContentItemDescription}>
+          <div className={styles.blockContentItemDescriptionHero}>
+            <h3 className="global-hero-glow-green m-0 p-0">{title}</h3>
+          </div>
+          <p className="m-0 p-0">{paragraph}</p>
+        </div>
+      </div>
+    )
   }
 
   componentDidMount() {
@@ -46,10 +65,21 @@ class ChatbotPage extends React.Component {
             </div>
           </div>
         </div>
+        <div className={styles.blockContent}>
+          <div className="container">
+            {this.pageItem(false, false, "Expert at handing over to the right recruiter", "Recruitment is a profoundly human art, which requires intuition, empathy and salesmanship. Our chatbots do not pretend to outperform human recruiters on any of these, but they assist for the part of the conversation which is standardized and repeatable. Using certain NLP triggers, our AI-powered chatbots recognize conversations which should deserve the attention of the recruiters.")}
+            {this.pageItem(true, conversation, "Start a conversation anywhere, anytime", "We make candidates’ lives easier by meeting them where they are. Our chatbots can engage and keep conversation going on the communication channels your candidates are already using, including career sites, job landing pages, SMS, Facebook Messenger, WhatsApp, WeChat, Line Messenger, and Slack.")}
+            {this.pageItem(false, context, "Context-aware for seamless experiences", "Context is the secret behind any great conversation. When it comes to the many conversation paths in a candidate’s journey, there are a lot of intermediary steps than apply and hire. Talkpush has mastered the art of maintaining the context in conversations as a way to optimize every step of the candidate journey, from click to hire, all the way until their first day on the job.")}
+            {this.pageItem(true, moreThanWords, "More than words", "Don’t limit yourself to text conversations. Our chatbots usual rich communications to engage with audiences and can collect audio responses, pictures, videos, documents, and any other type of content that helps candidates get to the next stage in the recruitment process. Using voice transcription technology, our chatbots can continue a conversation with the audio input, while capturing rich data about the candidate’s communication skills.")}
+          </div>
+        </div>
+        <div className={styles.blockBot}>
+
+        </div>
         <div className={styles.blockChat}>
           <div className="container">
             <div className={styles.blockChatLogo}>
-              <img src={logoIcon} alt="Logo"/>
+              <img src={logoIcon} alt="Logo" />
             </div>
             <div className={styles.blockChatTitle}>Talk to the bot</div>
             <div className={styles.blockChatMessage}>Feel free to ask Stanley our FAQ bot any question you may have about how to use conversational intelligence in your recruitment process:</div>
