70cde82fdb87bee10dc3e2a56f7c3dea326defa6
Bos Eriko <bos.eriko@gmail.com>
2018-08-14 14:34:40 +0800

    Fixed the links on the crm page

--- src/pages-split/index/talk-to-your-candidate.js
+++ src/pages-split/index/talk-to-your-candidate.js
@@ -18,7 +18,7 @@ const TalkToYourCandidateIndexPageSplit = () => (
           <h1 className="global-hero-glow-white text-uppercase m-0 p-0">Talk To Your</h1>
           <h1 className="global-hero-glow-green text-uppercase m-0 p-0">Candidate</h1>
           <p>Recruiting is pretty simple: get your recruiters and your candidates to talk to each other and watch good things happen. Talkpush is the first conversation-driven Candidate Relationship Management (CRM) system. On Talkpush, recruiters spend more time talking to qualified candidates, which translates into a better candidate experience and huge reductions in cost-per-hire and time-to-fill.</p>
-          <div className={styles.bigButtonHolder}><a onClick={calendlyPopUp} href="#Request" className="global-button"><i className="fa fa-send-o fa-fw" aria-hidden="true"></i>Request a Demo</a></div>
+          <div className={styles.bigButtonHolder}><a onClick={calendlyPopUp} href="#" className="global-button"><i className="fa fa-send-o fa-fw" aria-hidden="true"></i>Request a Demo</a></div>
           <div className={styles.otherButtonHolder}>
             <Link activeClassName="activePage" to="/crm" className="global-button"><img src={crmIcon} alt="CRM" />Discover our CRM Features</Link>
             <a href="https://m.me/774293282630560?ref=apply_for_position(2667)" target="_blank" className="global-button"><img src={chatbotIcon} alt="Chatbot" />Check our Chatbot</a>
--- src/pages/crm.js
+++ src/pages/crm.js
@@ -24,7 +24,11 @@ const pageVariables = {
   pageDescription: 'Candidate Relationship Management for the messaging age',
 }
 
-const crmItem = (reverse, photo, title, paragraph, icon, linkDestination, linkText) => {
+const calendlyPopUp = () => {
+  Calendly.showPopupWidget('https://calendly.com/talkpush-demo/talkpush-intro-demo/');
+}
+
+const crmItem = (reverse, photo, title, paragraph, icon, linkDestination, linkText, calendly) => {
   return (
     <div className={styles.blockContentItem} style={reverse === true ? { flexDirection: "row-reverse" } : null}>
       <div className={reverse === true ? styles.blockContentItemImageReverse : styles.blockContentItemImage}>
@@ -42,7 +46,7 @@ const crmItem = (reverse, photo, title, paragraph, icon, linkDestination, linkTe
         <p className="m-0 p-0">{paragraph}</p>
         {linkText ?
           <div className={reverse === true ? styles.blockContentItemDescriptionLinkReverse : styles.blockContentItemDescriptionLink}>
-            <a className="global-button" href={linkDestination}>{linkText}</a>
+            <a onClick={calendly ? calendly : null} target={calendly ? null: "_blank" } className="global-button" href={linkDestination}>{linkText}</a>
           </div>
           : null}
       </div>
@@ -66,20 +70,20 @@ const CrmPage = () => (
     </div>
     <div className={styles.blockContent}>
       <div className="container">
-        {crmItem(false, conversationsImage, "Conversations over forms", "Talkpush captures information from resumes or forms, but Talkpush goes beyond other recruitment systems. With the job application process handled like a conversation, your candidates can begin their journey with a simple “hello”. Conversations keep them engaged and improve your conversion rates.", overFormsIcon, "#", "See it in action")}
-        {crmItem(true, talentTalkImage, "Let the talent talk", "Resumes are nice, but they only tell a small part of the story. If you want to know a candidate, you need to listen to them. By asking candidates to record audio and video responses during the application, recruiters can evaluate communications and language skills as well as the ability to think on their feet.", talentTalkIcon, "#", "Book a Demo")}
-        {crmItem(false, multichannelImage, "Multichannel", "Don’t make it harder for good talent to find you. Open all communications channels: job boards, phone lines, social media... On Talkpush, you can chat with thousands of candidates via SMS, Facebook or voice and automate as much or as little of that conversation as you’d like.", multichannelIcon, "#", "Talk to us about Marketing")}
-        {crmItem(true, speedImage, "Speed is everything", "In recruitment, speed is everything. A candidate in high demand will most likely end up working for the employer who has responded first. Talkpush helps recruiters minimize the response time to candidates by 80%. Every time a candidate moves to a new stage of the process, new actions are triggered ensuring that the best candidates are fast tracked and that every candidate is kept informed.", speedIcon, "#", false)}
-        {crmItem(false, buildingImage, "Building and maintaining relationships at scale", "With inboxes polluted with spam and poorly targeted offers, it’s no wonder that email marketing is on its last legs... Talkpush allows you to manage your talent pool via to personalized one-on-one conversations over SMS or messaging apps.", buildingIcon, "#", false)}
-        {crmItem(true, aiImage, "Artificial Intelligence", "What if you could focus 100% of your time on the qualified candidates? That's the promise of the SmartFilter which lets you eliminate unsuitable candidates automatically right when they apply.", aiIcon, "#", false)}
-        {crmItem(false, chromeImage, "Chrome Plugin", "If your team spends time searching applicants online, whether it be on LinkedIn, Facebook Groups, or job boards like Indeed or Monster, they will love the Talkpush Chrome Plugin, from which they can add candidates to campaign in a fraction of the time.", chromeIcon, "#", "Download Here")}
-        {crmItem(true, recordImage, "Record everything", "With Talkpush your recruiters can make & receive calls with all candidates at the click of a button, moving their conversation past Emails & SMS. With every call recorded and all activity tracked, Talkpush is a tool for you to improve your technique and drive productivity.", recordIcon, "#", false)}
-        {crmItem(false, apiImage, "API Integration", "If your team spends time searching applicants online, whether it be on LinkedIn, Facebook groups, or job boards like Indeed or Monster, they will love the Talkpush Chrome Plugin, from which they can add candidates to campaign in a fraction of the time.", false, "#", false)}
+        {crmItem(false, conversationsImage, "Conversations over forms", "Talkpush captures information from resumes or forms, but Talkpush goes beyond other recruitment systems. With the job application process handled like a conversation, your candidates can begin their journey with a simple “hello”. Conversations keep them engaged and improve your conversion rates.", overFormsIcon, "#", "See it in action", calendlyPopUp)}
+        {crmItem(true, talentTalkImage, "Let the talent talk", "Resumes are nice, but they only tell a small part of the story. If you want to know a candidate, you need to listen to them. By asking candidates to record audio and video responses during the application, recruiters can evaluate communications and language skills as well as the ability to think on their feet.", talentTalkIcon, "#", "Book a Demo", calendlyPopUp)}
+        {crmItem(false, multichannelImage, "Multichannel", "Don’t make it harder for good talent to find you. Open all communications channels: job boards, phone lines, social media... On Talkpush, you can chat with thousands of candidates via SMS, Facebook or voice and automate as much or as little of that conversation as you’d like.", multichannelIcon, "#", "Talk to us about Marketing", calendlyPopUp)}
+        {crmItem(true, speedImage, "Speed is everything", "In recruitment, speed is everything. A candidate in high demand will most likely end up working for the employer who has responded first. Talkpush helps recruiters minimize the response time to candidates by 80%. Every time a candidate moves to a new stage of the process, new actions are triggered ensuring that the best candidates are fast tracked and that every candidate is kept informed.", speedIcon, "#", false, false)}
+        {crmItem(false, buildingImage, "Building and maintaining relationships at scale", "With inboxes polluted with spam and poorly targeted offers, it’s no wonder that email marketing is on its last legs... Talkpush allows you to manage your talent pool via to personalized one-on-one conversations over SMS or messaging apps.", buildingIcon, "#", false, false)}
+        {crmItem(true, aiImage, "Artificial Intelligence", "What if you could focus 100% of your time on the qualified candidates? That's the promise of the SmartFilter which lets you eliminate unsuitable candidates automatically right when they apply.", aiIcon, "#", false, false)}
+        {crmItem(false, chromeImage, "Chrome Plugin", "If your team spends time searching applicants online, whether it be on LinkedIn, Facebook Groups, or job boards like Indeed or Monster, they will love the Talkpush Chrome Plugin, from which they can add candidates to campaign in a fraction of the time.", chromeIcon, "https://chrome.google.com/webstore/detail/talkpush/hikdfefkfklgdnhgkdfdddeaicbgebbg?hl=en-US", "Download Here", false)}
+        {crmItem(true, recordImage, "Record everything", "With Talkpush your recruiters can make & receive calls with all candidates at the click of a button, moving their conversation past Emails & SMS. With every call recorded and all activity tracked, Talkpush is a tool for you to improve your technique and drive productivity.", recordIcon, "#", false, false)}
+        {crmItem(false, apiImage, "API Integration", "If your team spends time searching applicants online, whether it be on LinkedIn, Facebook groups, or job boards like Indeed or Monster, they will love the Talkpush Chrome Plugin, from which they can add candidates to campaign in a fraction of the time.", false, "#", false, false)}
       </div>
     </div>
     <div className={styles.blockCTA}>
       <h3 className="global-hero-glow-green m-0 p-0">Ready to get started?</h3>
-      <a href="#" className="global-button">
+      <a href="#" onClick={calendlyPopUp} className="global-button">
         <i className="fa fa-send-o fa-fw" aria-hidden="true"></i>
         <span>Book a Demo</span>
       </a>
