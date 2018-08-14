bc824f87cf22b6b11e2a7a862775c523bd121955
Bos Eriko <bos.eriko@gmail.com>
2018-08-14 18:44:10 +0800

    Added the modal to the jobs page

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styles from '../pages-style/jobs.module.scss'
+import Modal from '../components/modal'
 // import hiringIcon from '../pages-images/jobs/hiring.svg'
 // import cultureIcon from '../pages-images/jobs/culture.svg'
 // import locationIcon from '../pages-images/jobs/location.svg'
 
 class JobsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalStatus: false
+    }
+  }
+
+  openModal = (e) => {
+    e.preventDefault();
+    document.getElementsByTagName("body")[0].classList.add("noScroll")
+    this.setState({
+      modalStatus: true
+    })
+  }
+
+  closeModal = (e) => {
+    e.preventDefault();
+    document.getElementsByTagName("body")[0].classList.remove("noScroll")
+    this.setState({
+      modalStatus: false
+    })
+  }
+
   pageVariables = {
     pageCode: 'jobs',
     pageDescription: 'Ready to accelerate recruitment? Talkpush is the leading productivity platform and the most high-volume chatbot for hiring talent via Facebook, WeChat and messaging.',
@@ -41,21 +65,22 @@ class JobsPage extends React.Component {
           <meta name='description' content={this.pageVariables.pageDescription} />
           <meta property='og:description' content={this.pageVariables.pageDescription} />
         </Helmet>
+        {this.state.modalStatus ? <Modal closeModal={this.closeModal} type="gallery" maxWidth="800" title="Office Life" content="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." /> : null}
         <div className={styles.blockHeader}>
           <div className={styles.blockHeaderContent}>
             <div className={styles.blockHeaderContentFill}></div>
             <h1 className="global-hero-glow-white p-0 m-0">We're Hiring</h1>
             <p className="p-0">Thinking about working at Talkpush? We want to learn more about you so we can decide if there is a potential career for you at Talkpush. Talk to our recruitment agent Stanley below and start that conversation NOW.</p>
             <div className={styles.blockHeaderContentLink}>
-              <a href="#" className="global-button">
+              <a href="#" onClick={this.openModal} className="global-button">
                 <i className="fa fa-users fa-fw" aria-hidden="true"></i>
                 <span>Hiring Process</span>
               </a>
-              <a href="#" className="global-button">
+              <a href="#" onClick={this.openModal} className="global-button">
                 <i className="fa fa-coffee fa-fw" aria-hidden="true"></i>
                 <span>Our Culture</span>
               </a>
-              <a href="#" className="global-button">
+              <a href="#" onClick={this.openModal} className="global-button">
                 <i className="fa fa-globe fa-fw" aria-hidden="true"></i>
                 <span>Our Location</span>
               </a>
