9f210ec3853c00d6ef153b225a9ddbc01fc9cf19
Bos Eriko <bos.eriko@gmail.com>
2018-08-30 14:22:44 +0800

    Added alorica

--- src/pages/customers.js
+++ src/pages/customers.js
@@ -13,6 +13,8 @@ import psgNeon from '../pages-images/customers/psg/neon.svg'
 import psgBlack from '../pages-images/customers/psg/black.svg'
 import sykesNeon from '../pages-images/customers/sykes/neon.png'
 import sykesBlack from '../pages-images/customers/sykes/black.png'
+import aloricaNeon from '../pages-images/customers/alorica/neon.svg'
+import aloricaBlack from '../pages-images/customers/alorica/black.svg'
 
 class CustomersPage extends React.Component {
   pageVariables = {
@@ -90,6 +92,7 @@ class CustomersPage extends React.Component {
               {this.blockItem("alexander", alexanderNeon, alexanderBlack, "Alexander Mann", "South East Asia", false, false)}
               {this.blockItem("psg", psgNeon, psgBlack, "PSG Global Solutions", "Global", false, false)}
               {this.blockItem("sykes", sykesNeon, sykesBlack, "Sykes", "Philippines, Latin America", false, "https://medium.com/@perrine.issachar/a7641b6e1285")}
+              {this.blockItem("alorica", aloricaNeon, aloricaBlack, "Alorica", "Global", "ciSB3gMpH_g", false)}
             </div>
           </div>
         </div>
