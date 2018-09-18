403df9bd7c5985cca7a2fbcf13d284205f98fcb4
Bos Eriko <bos.eriko@gmail.com>
2018-09-18 18:20:23 +0800

    Changed a founder to Max

--- src/pages/our-style.js
+++ src/pages/our-style.js
@@ -44,7 +44,7 @@ const OurStylePage = ({ siteTitle }) => (
           </div>
         </div>
         <div className={styles.blockContentVideoHolder}>
-          <h2 className="global-hero-glow-green m-0 p-0">A word from our Founder</h2>
+          <h2 className="global-hero-glow-green m-0 p-0">A word from Max</h2>
           <div className={styles.blockContentVideoHolderVideo}>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/gNISmYgHKC4" frameBorder="0" allowFullScreen></iframe>
           </div>
