3b7eb0212dc097181d3ed6da7da8dde46ec87508
Bos Eriko <bos.eriko@gmail.com>
2018-09-21 19:25:04 +0800

    Made our-team a statefull component with <RowItems /> in it

--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import styles from '../pages-style/our-team.module.scss'
 import defaultPicture from '../pages-images/our-team/default.png'
 import Link from 'gatsby-link'
+import Modal from '../components/modal'
 
 const pageVariables = {
   pageCode: 'our-team',
@@ -10,134 +11,103 @@ const pageVariables = {
   pageTitle: 'Our Team',
 }
 
-
-const OurTeamPage = ({ siteTitle }) => (
-  <div className={styles.block}>
-    <Helmet>
-      <title>{pageVariables.pageTitle} - {siteTitle}</title>
-      <meta name='description' content={pageVariables.pageDescription} />
-      <meta property='og:description' content={pageVariables.pageDescription} />
-    </Helmet>
-    <div className={styles.blockHeader}></div>
-    <div className={styles.blockExperiences}>
-      <div className="container">
-        <h2 className="global-hero-glow-white m-0 p-0" style={{ textShadow: "0 0 20px #e800ff" }}>Our Experiences</h2>
-        <div className={styles.blockExperiencesDivider}>
-          <div className={styles.blockExperiencesDividerLeft}>
-            <div className={styles.blockExperiencesDividerLeftImage}>
-              <img src={defaultPicture} alt="Yuan" />
+class OurTeamPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalStatus: false,
+    }
+  }
+  openModal = (e) => {
+    e.preventDefault();
+    document.getElementsByTagName("body")[0].classList.add("noScroll")
+    this.setState({
+      modalStatus: true
+    })
+  }
+  closeModal = (e) => {
+    e.preventDefault();
+    document.getElementsByTagName("body")[0].classList.remove("noScroll")
+    this.setState({
+      modalStatus: false
+    })
+  }
+  RowItem = () => {
+    return (
+      <div className={styles.blockTeamMembersRowItem}>
+        <div className={styles.blockTeamMembersRowItemImage}>
+          <img src={defaultPicture} alt="Default" />
+        </div>
+        <h4 className="m-0 p-0">Name of Employee</h4>
+        <span>Current Position</span>
+        <a href="#" onClick={this.openModal} className="global-button text-lowercase">Read More</a>
+      </div>
+    )
+  }
+  render() {
+    const { RowItem } = this;
+    return (
+      <div className={styles.block}>
+        <Helmet>
+          <title>{pageVariables.pageTitle} - {this.props.siteTitle}</title>
+          <meta name='description' content={pageVariables.pageDescription} />
+          <meta property='og:description' content={pageVariables.pageDescription} />
+        </Helmet>
+        {this.state.modalStatus && <Modal closeModal={this.closeModal} maxWidth="400" title={"title"} content={"content"} />}
+        <div className={styles.blockHeader}></div>
+        <div className={styles.blockExperiences}>
+          <div className="container">
+            <h2 className="global-hero-glow-white m-0 p-0" style={{ textShadow: "0 0 20px #e800ff" }}>Our Experiences</h2>
+            <div className={styles.blockExperiencesDivider}>
+              <div className={styles.blockExperiencesDividerLeft}>
+                <div className={styles.blockExperiencesDividerLeftImage}>
+                  <img src={defaultPicture} alt="Yuan" />
+                </div>
+              </div>
+              <div className={styles.blockExperiencesDividerRight}>
+                <div className={styles.blockExperiencesDividerRightNamePosition}>
+                  <h3 className="global-hero-glow-green m-0 p-0">Yuan Yim</h3>
+                  <span>Head of Resource and Community</span>
+                </div>
+                <p className="m-0">I have always wanted to show people what it’s like to work for a company like ours and to share my experience of the start-up adventure, so I’ve put together this vlog, which I hope you’ll get some inspiration from and maybe even get you excited about exploring career opportunities at Talkpush.</p>
+                <div className={styles.blockExperiencesDividerRightReadMore}>
+                  <Link to="/our-team/yuan" className="global-button text-lowercase">Read More</Link>
+                </div>
+              </div>
             </div>
           </div>
-          <div className={styles.blockExperiencesDividerRight}>
-            <div className={styles.blockExperiencesDividerRightNamePosition}>
-              <h3 className="global-hero-glow-green m-0 p-0">Yuan Yim</h3>
-              <span>Head of Resource and Community</span>
+        </div>
+        <div className={styles.blockTeam}>
+          <div className="container">
+            <h2 className="global-hero-glow-green m-0 p-0" style={{ textShadow: 'none' }}>Meet the team</h2>
+            <div className={styles.blockTeamMembers}>
+              <div className={styles.blockTeamMembersRow}>
+                <RowItem />
+                <RowItem />
+                <RowItem />
+              </div>
+              <div className={styles.blockTeamMembersRow}>
+                <RowItem />
+                <RowItem />
+                <RowItem />
+              </div>
             </div>
-            <p className="m-0">I have always wanted to show people what it’s like to work for a company like ours and to share my experience of the start-up adventure, so I’ve put together this vlog, which I hope you’ll get some inspiration from and maybe even get you excited about exploring career opportunities at Talkpush.</p>
-            <div className={styles.blockExperiencesDividerRightReadMore}>
-              <Link to="/our-team/yuan" className="global-button text-lowercase">Read More</Link>
+          </div>
+        </div>
+        <div className={styles.blockJoin}>
+          <div className="container">
+            <div className={styles.blockJoinBox}>
+              <div className={styles.blockJoinBoxDetails}>
+                <h3>Join our growing team</h3>
+                <p className="m-0 p-0">Join Talkpush and let's make recruitment fun together</p>
+                <Link to="/jobs">Careers</Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className={styles.blockTeam}>
-      <div className="container">
-        <h2 className="global-hero-glow-green m-0 p-0" style={{ textShadow: 'none' }}>Meet the team</h2>
-        <div className={styles.blockTeamMembers}>
-          <div className={styles.blockTeamMembersRow}>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-          </div>
-          <div className={styles.blockTeamMembersRow}>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-          </div>
-          <div className={styles.blockTeamMembersRow}>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-            <div className={styles.blockTeamMembersRowItem}>
-              <div className={styles.blockTeamMembersRowItemImage}>
-                <img src={defaultPicture} alt="Default" />
-              </div>
-              <h4 className="m-0 p-0">Name of Employee</h4>
-              <span>Current Position</span>
-              <a href="#" className="global-button text-lowercase">Read More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className={styles.blockJoin}>
-      <div className="container">
-        <div className={styles.blockJoinBox}>
-          <div className={styles.blockJoinBoxDetails}>
-            <h3>Join our growing team</h3>
-            <p className="m-0 p-0">Join Talkpush and let's make recruitment fun together</p>
-            <Link to="/jobs">Careers</Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default OurTeamPage
