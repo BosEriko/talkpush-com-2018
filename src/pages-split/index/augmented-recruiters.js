8d5c11664c68a8d9e8be52832e3af41520612db1
Bos Eriko <bos.eriko@gmail.com>
2018-09-21 13:19:18 +0800

    Added images on the testimonial

--- src/pages-split/index/augmented-recruiters.js
+++ src/pages-split/index/augmented-recruiters.js
@@ -2,6 +2,12 @@ import React from 'react'
 import styles from './style/augmented-recruiters.module.scss'
 import userIcon from './images/augmented-recruiters/user.svg'
 
+import keithCheung from './images/augmented-recruiters/keith_cheung.jpeg'
+import lynBautista from './images/augmented-recruiters/lyn_bautista.png'
+import matthewJohnson from './images/augmented-recruiters/matthew_johnson.jpeg'
+import pingStaCatalina from './images/augmented-recruiters/ping_sta_catalina.jpeg'
+import marvinVinluan from './images/augmented-recruiters/marvin_vinluan.png'
+
 class AugmentedRecruitersIndexPageSplit extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +15,7 @@ class AugmentedRecruitersIndexPageSplit extends React.Component {
       animationWorking: true,
     }
   }
-  quoteBlock(videoID, message, name, position, company) {
+  quoteBlock(videoID, message, name, position, company, picture = userIcon) {
     return (
       <li className="testimonial-section-divide-item">
         <div className={styles.blockDivideItemVideo}>
@@ -20,7 +26,7 @@ class AugmentedRecruitersIndexPageSplit extends React.Component {
             {message}
           </div>
           <div className={styles.blockDivideItemDetailsUser}>
-            <img src={userIcon} className={styles.blockDivideItemDetailsUserPicture} alt={name} title={name} />
+            <img src={picture} className={styles.blockDivideItemDetailsUserPicture} alt={name} title={name} />
             <div className={styles.blockDivideItemDetailsUserDetails}>
               <div className={styles.blockDivideItemDetailsUserDetailsName}>{name}</div>
               <div className={styles.blockDivideItemDetailsUserDetailsPosition}>{position}</div>
@@ -78,11 +84,11 @@ class AugmentedRecruitersIndexPageSplit extends React.Component {
           <h4 className="p-0">They build more engaging experiences and increase recruitment outputs through automation and AI</h4>
           <ul className={styles.blockDivide} id="testimonial-section-divide" onMouseOver={() => this.setState({ animationWorking: false })} onMouseOut={() => this.setState({ animationWorking: true })}>
 
-            {this.quoteBlock("ciSB3gMpH_g", "With Talkpush, our hire rate has gone up considerably and we are able to handle much more volume with our existing team.", "Marvin Vinluan", "Director / Strategic Sourcing / Recruitment Marketing", "Alorica")}
-            {this.quoteBlock("abg2U9DuAiA", "I love Talkpush because it allows access to multiple sourcing channels in one platform.", "Ping Sta. Catalina", "Client Services Manager", "Alexander Mann Solutions")}
-            {this.quoteBlock("2cj1nKFZQDA", "We partnered with Talkpush to get ahead on social media, use Facebook as a sourcing channel and drive a faster candidate response time, lowering our cost per hire by 60%.", "Matthew Johnson", "Global Director", "Valor Global Services")}
-            {this.quoteBlock("Vpu8HkBHShw", "We partnered with Talkpush for a fast and easy recruitment experience, allowing us to engage with twice as many people.", "Lyn Bautista", "Sourcing and Branding Manager", "[24]7.ai")}
-            {this.quoteBlock("nFRDqo_rc3o", "We have been using Talkpush since 2014 and it has streamlined our recruitment tremendously. We are able to spend more time with the quality candidates and have increased our interview to hire ratio.", "Keith Cheung", "Head of Talent Acquisition", "Sheraton and St. Regis Macau")}
+            {this.quoteBlock("ciSB3gMpH_g", "With Talkpush, our hire rate has gone up considerably and we are able to handle much more volume with our existing team.", "Marvin Vinluan", "Director / Strategic Sourcing / Recruitment Marketing", "Alorica", marvinVinluan)}
+            {this.quoteBlock("abg2U9DuAiA", "I love Talkpush because it allows access to multiple sourcing channels in one platform.", "Ping Sta. Catalina", "Client Services Manager", "Alexander Mann Solutions", pingStaCatalina)}
+            {this.quoteBlock("2cj1nKFZQDA", "We partnered with Talkpush to get ahead on social media, use Facebook as a sourcing channel and drive a faster candidate response time, lowering our cost per hire by 60%.", "Matthew Johnson", "Global Director", "Valor Global Services", matthewJohnson)}
+            {this.quoteBlock("Vpu8HkBHShw", "We partnered with Talkpush for a fast and easy recruitment experience, allowing us to engage with twice as many people.", "Lyn Bautista", "Sourcing and Branding Manager", "[24]7.ai", lynBautista)}
+            {this.quoteBlock("nFRDqo_rc3o", "We have been using Talkpush since 2014 and it has streamlined our recruitment tremendously. We are able to spend more time with the quality candidates and have increased our interview to hire ratio.", "Keith Cheung", "Head of Talent Acquisition", "Sheraton and St. Regis Macau", keithCheung)}
 
           </ul>
         </div>
